43d090dbf0af11d51516975d7cf96ba1ac5d1e41
Spolpinka <a421243266@gmail.com>
2023-04-10 12:28:12 +0300

    first commit

---
+++ src/main/java/pro/sky/testjunit/TestJunitApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.testjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJunitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJunitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/testjunit/TestJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.testjunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJunitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
