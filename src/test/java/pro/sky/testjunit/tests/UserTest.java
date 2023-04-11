9a32ab82fe548d23ffe3d968078f9af054a51143
Spolpinka <a421243266@gmail.com>
2023-04-11 08:13:40 +0300

    первые 3 теста + 4ый тест + параметризированный тест

--- src/test/java/pro/sky/testjunit/tests/UserTest.java
+++ src/test/java/pro/sky/testjunit/tests/UserTest.java
@@ -2,6 +2,8 @@ package pro.sky.testjunit.tests;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import pro.sky.testjunit.model.User;
 
 import static org.junit.jupiter.api.Assertions.*;
---
+++ src/test/java/pro/sky/testjunit/tests/UserTestParams.java
@@ -0,0 +1,31 @@
+package pro.sky.testjunit.tests;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import pro.sky.testjunit.model.User;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static pro.sky.testjunit.constants.UserTestConstants.EXPECTED_EMAIL;
+
+class UserTestParams {
+
+    public static Stream<Arguments> paramsForTests() {
+        return Stream.of(
+                Arguments.of("user1", "mail@mail.com"),
+                Arguments.of("user2", "example@gmail.com"),
+                Arguments.of("user3", EXPECTED_EMAIL)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("paramsForTests")
+    public void testUserCreation(String login, String email) {
+        User user = new User(login, email);
+        assertEquals(login, user.getLogin());
+        assertEquals(email, user.getEmail());
+    }
+
+}
