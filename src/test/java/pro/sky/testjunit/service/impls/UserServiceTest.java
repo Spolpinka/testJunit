0346976630e8aa67514b1509b98df67108bef00f
Spolpinka <a421243266@gmail.com>
2023-04-19 12:56:52 +0300

    Mock реализован

--- src/test/java/pro/sky/testjunit/service/impls/UserServiceTest.java
+++ src/test/java/pro/sky/testjunit/service/impls/UserServiceTest.java
@@ -1,10 +1,13 @@
 package pro.sky.testjunit.service.impls;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pro.sky.testjunit.model.User;
 import pro.sky.testjunit.repository.UserDao;
 import pro.sky.testjunit.service.UserService;
 
@@ -23,8 +26,26 @@ class UserServiceTest {
     @InjectMocks
     private UserServiceImpl out;
 
+    private User user1;
+    private User user2;
+    private User notExistedUser;
+
     @BeforeEach
     public void init() {
+        MockitoAnnotations.openMocks(this);
+        user1 = new User("MrFirst", "1234@1234.com");
+        user2 = new User("MrSecond", "1234@1234.com");
+
+        notExistedUser = new User("MrNotExisted", "mail@mail.com");
+
     }
 
+    @Test
+    public void checkUserExistTest() {
+        when(daoMock.getUserByName("MrFirst")).thenReturn(user1);
+
+        assertTrue(out.checkUserExist(user1));
+        assertTrue(out.checkUserExist(user2));
+        assertFalse(out.checkUserExist(notExistedUser));
+    }
 }
